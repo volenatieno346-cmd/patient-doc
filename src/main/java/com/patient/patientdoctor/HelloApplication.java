d46385a8a7462db406fef1175b63fa828e037f2d
volenatieno346-cmd <volenatieno346@gmail.com>
2026-09-15 17:51:21 +0300

    Validation OF passwords

--- src/main/java/com/patient/patientdoctor/HelloApplication.java
+++ src/main/java/com/patient/patientdoctor/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Patient_Doctor Apointment");
+        stage.setTitle("Patient_Doctor Appointment");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/patient/patientdoctor/loginController.java
+++ src/main/java/com/patient/patientdoctor/loginController.java
@@ -1,4 +1,57 @@
 package com.patient.patientdoctor;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+
 public class loginController {
+    @FXML
+    private Label error;
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private Label passwordwarning;
+    @FXML private Label employeeidwarning;
+    @FXML private Label usernamewsrning;
+    @FXML private static final String PasswordRegex= "^. {8,}$";
+    @FXML public void initialize(){
+        password.textProperty().addListener((obs,oldVal,newVal) ->
+                validatePassword(newVal));
+
+    }
+
+    @FXML
+    protected void onLoginClick() {
+        String usernameText = username.getText();
+        if(usernameText.isEmpty() && password.getText().isEmpty()){
+            error.setText("Please enter your name!!");
+        }
+        else{
+            if(!isPasswordValidation()){
+                error.setText("Please enter a strong password!");
+            }
+            //saving it to the database.
+            error.setText("the login username is valid");
+        }
+    }
+    @FXML public void validatePassword(String password){
+        if(password.length()<8){
+            passwordwarning.setText("password should have eight and above digits");
+        }
+        if (!password.matches(".*\\d*.*")) {
+            passwordwarning.setText("password should contain at least one digit");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            passwordwarning.setText("password should contain at least one uppercase letter");
+
+        }
+         if(password.matches(".*[A-Z].*") && password.matches(".*\\d*.*") && password.length()>=8){
+            passwordwarning.setText("");}
+    }
+    @FXML public boolean isPasswordValidation(){
+        return password.getText().matches(PasswordRegex);
+
+    }
+
+
 }
