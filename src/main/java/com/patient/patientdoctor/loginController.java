49d84646aeb8903079d7bab2561484f39b741718
volenatieno346-cmd <volenatieno346@gmail.com>
2026-09-16 21:48:00 +0300

    functional login necessary validation

--- src/main/java/com/patient/patientdoctor/loginController.java
+++ src/main/java/com/patient/patientdoctor/loginController.java
@@ -1,6 +1,7 @@
 package com.patient.patientdoctor;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
@@ -10,15 +11,31 @@ public class loginController {
     private Label error;
     @FXML private TextField username;
     @FXML private TextField password;
+    @FXML private TextField employeeid;
     @FXML private Label passwordwarning;
     @FXML private Label employeeidwarning;
-    @FXML private Label usernamewsrning;
+    @FXML private Label  usernamewarning;
+    @FXML private Button clear;
     @FXML private static final String PasswordRegex= "^. {8,}$";
     @FXML public void initialize(){
         password.textProperty().addListener((obs,oldVal,newVal) ->
                 validatePassword(newVal));
+        username.textProperty().addListener((obs,oldVal,newVal) ->
+                validateUsername(newVal));
+        employeeid.textProperty().addListener((obs,oldVal,newVal)->
+                validateEmployeeId(newVal));
+
 
     }
+
+    private void validateEmployeeId(String eId) {
+        if(!eId.matches("^Emp/[A-Z]/\\d{4}/\\d{4}$\n")){
+            employeeidwarning.setText("Wrong format please try again. Eg Emp/D/1234/2009");
+        }else{
+            employeeidwarning.setText("");
+        }
+    }
+
 
     @FXML
     protected void onLoginClick() {
@@ -48,9 +65,22 @@ public class loginController {
          if(password.matches(".*[A-Z].*") && password.matches(".*\\d*.*") && password.length()>=8){
             passwordwarning.setText("");}
     }
+    @FXML public void validateUsername(String username){
+        if(username.isEmpty()){
+            usernamewarning.setText("Please fill in your username!!");
+        }else{
+            usernamewarning.setText("");
+        }
+
+    }
     @FXML public boolean isPasswordValidation(){
         return password.getText().matches(PasswordRegex);
 
+    }
+    @FXML public void clearbtn(){
+        username.setText("");
+        employeeid.setText("");
+        password.setText("");
     }
 
 
